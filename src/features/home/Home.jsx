b1ec1a214bfe49d22f8e6697821cd16a6e2e2746
Abraham Cabrera <testingdfall@gmail.com>
2026-02-18 09:02:15 +0100

    feat: Add a new Disciplines section to the Home page, including its components and styling.

--- src/features/home/Home.jsx
+++ src/features/home/Home.jsx
@@ -4,12 +4,14 @@ import Stats from './components/Stats/Stats'
 import SuccessStories from './components/SuccessStories/SuccessStories'
 import SuccessHistoryCTA from './components/SuccessHistoryCTA/SuccessHistoryCTA'
 import VipHorses from './components/VipHorses/VipHorses'
+import Disciplines from './components/Disciplines/Disciplines'
 
 const Home = () => {
     return (
         <>
             <Hero />
             <Stats />
+            <Disciplines />
             <VipHorses />
             <SuccessStories />
             <SuccessHistoryCTA />
