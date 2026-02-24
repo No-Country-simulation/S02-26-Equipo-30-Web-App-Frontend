7a082d7a2728e726a190194a483bbff8102d2554
Abraham Cabrera <testingdfall@gmail.com>
2026-02-24 21:26:27 +0100

    feat: Implement login and register pages with their respective styles and integrate them into the application's routing.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -7,6 +7,9 @@ import Premium from '@features/premium/Premium.jsx';
 import HowItWorks from '@features/how-it-works/HowItWorks.jsx';
 import TrustAndSecurity from '@features/trust-and-security/TrustAndSecurity.jsx';
 import Pricing from '@features/pricing/Pricing.jsx';
+import Login from '@features/auth/Login.jsx';
+import Register from '@features/auth/Register.jsx';
+import Verification from '@features/auth/Verification.jsx';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -20,6 +23,9 @@ function App() {
         <Route path="/como-funciona" element={<HowItWorks />} />
         <Route path="/confianza-y-seguridad" element={<TrustAndSecurity />} />
         <Route path="/planes" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registro" element={<Register />} />
+        <Route path="/verificar" element={<Verification />} />
       </Routes>
     </PublicView>
   );
