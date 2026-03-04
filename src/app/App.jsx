2eacabd8545d53cef3944d4e35e7c499be492904
Abraham Cabrera <testingdfall@gmail.com>
2026-03-04 08:35:57 +0100

    feat: Initialize application with React Router, authentication context, and protected routes.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -21,32 +21,34 @@ import HorseForm from '@features/horse-management/HorseForm.jsx';
 
 import { Routes, Route } from 'react-router-dom';
 import { AuthProvider } from '../shared/context/AuthContext';
+import ProtectedRoute from '@/shared/common/ProtectedRoute';
 
 function App() {
   return (
     <AuthProvider>
       <PublicView>
         <Routes>
+          {/* Rutas Públicas */}
           <Route path="/" element={<Home />} />
           <Route path="/explorar" element={<Explore />} />
           <Route path="/detalle" element={<HorseDetails />} />
           <Route path="/login" element={<Login />} />
           <Route path="/registro" element={<Register />} />
           <Route path="/verificar" element={<Verification />} />
-          <Route path="/perfil" element={<Profile />} />
-          <Route path="/perfil/editar" element={<EditProfile />} />
-
-          <Route path="/confianza-seguridad" element={<TrustSafety />} />
-          <Route path="/sobre-nosotros" element={<AboutUs />} />
-          <Route path="/favoritos" element={<Favorites />} />
           <Route path="/como-funciona" element={<HowItWorks />} />
+          <Route path="/sobre-nosotros" element={<AboutUs />} />
           <Route path="/contacto" element={<Contact />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/compra" element={<PurchaseProcess />} />
-          <Route path="/chat" element={<Chat />} />
-          <Route path="/caballo/nuevo" element={<HorseForm />} />
-          <Route path="/caballo/editar/:id" element={<HorseForm />} />
+          <Route path="/confianza-seguridad" element={<TrustSafety />} />
 
+          {/* Rutas Protegidas */}
+          <Route path="/perfil" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+          <Route path="/perfil/editar" element={<ProtectedRoute><EditProfile /></ProtectedRoute>} />
+          <Route path="/favoritos" element={<ProtectedRoute><Favorites /></ProtectedRoute>} />
+          <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          <Route path="/compra" element={<ProtectedRoute><PurchaseProcess /></ProtectedRoute>} />
+          <Route path="/chat" element={<ProtectedRoute><Chat /></ProtectedRoute>} />
+          <Route path="/caballo/nuevo" element={<ProtectedRoute><HorseForm /></ProtectedRoute>} />
+          <Route path="/caballo/editar/:id" element={<ProtectedRoute><HorseForm /></ProtectedRoute>} />
         </Routes>
       </PublicView>
     </AuthProvider>
